5fae3855ed8c25e57ec3b96976fb667a5d7490d1
Alper Dom <h.alper.dom@gmail.com>
2016-11-01 14:53:27 +0300

    garden/public

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -22,7 +22,7 @@ class AuthController extends Controller
     */
 
     use AuthenticatesAndRegistersUsers, ThrottlesLogins;
-    protected $redirectPath = 'santiye';
+    protected $redirectPath = 'garden/public/santiye';
 
     /**
      * Create a new authentication controller instance.
